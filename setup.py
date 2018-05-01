56247aece13d0fd7a733185ed5ae74dda51d178e
Galen Li <allenlsj@umich.edu>
2018-04-30 23:36:02 -0400

    update release

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ except IOError:
     long_description = ""
 
 setup(
-    name="Spark-lean",
-    version="0.1",
-    description="A data cleaning framework for Spark",
+    name="spark_lean",
+    packages = ['spark_lean'],
+    version="0.3.2",
+    description="an interactive PySpark-based Data Cleaning Library",
     license="MIT",
     author="Shuaiji Li, Yicheng Pu, Qingtai Liu",
     author_email="sl6486@nyu.edu, yp653@nyu.edu, ql819@nyu.edu",
-    url = 'https://github.com/qltf8/1004_LPL_project',
-    download_url = 'https://github.com/qltf8/1004_LPL_project/archive/0.1.tar.gz',
-    packages=find_packages(),
+    url = 'https://github.com/allenlsj/Spark-lean',
+    download_url = 'https://github.com/allenlsj/Spark_lean/archive/0.3.tar.gz',
     install_requires=['pyspark'],
     long_description=long_description,
-    keywords = ['PySpark', 'Data Cleaning', 'Interactive']
-    classifiers=[
+    keywords = ['PySpark', 'Data Cleaning', 'Interactive'],
+    classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
